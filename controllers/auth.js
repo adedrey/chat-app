820609f71a31a5ef227986f180870fa693bab04c
Adedire Emmanuel <adedireemmanuel@gmail.com>
2022-03-20 09:36:00 +0100

    Added Comments to Code

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,11 +5,8 @@ const crypto = require("crypto");
 // Adds new user
 exports.postAddUser = async (req, res, next) => {
     try {
-        const name = req.body.name;
-        const email = req.body.email;
-        const username = req.body.username;
-        const password = req.body.password;
-        const confirm_password = req.body.confirm_password;
+        // Get all fields from the clients
+        const { name, email, username, password, confirm_password } = req.body;
         // Check if email already exist
         const checkEmail = await User.findOne({
             email: email
@@ -18,11 +15,13 @@ exports.postAddUser = async (req, res, next) => {
         const checkUsername = await User.findOne({
             username: username
         });
+        // return error if email exist
         if (checkEmail) {
             return res.status(401).json({
                 message: 'Email already exist!'
             })
         }
+        // returns error if username exist
         if (checkUsername) {
             return res.status(401).json({
                 message: 'Username already exist'
@@ -34,10 +33,12 @@ exports.postAddUser = async (req, res, next) => {
                 message: 'Passwords does not match!'
             })
         }
+        // Passport
         const saltHash = utils.genPassword(password);
         const salt = saltHash.salt;
         const hash = saltHash.hash;
 
+        // Save new User
         const newuser = await new User({
             name: name,
             email: email,
@@ -46,6 +47,8 @@ exports.postAddUser = async (req, res, next) => {
             salt: salt
         })
         const user = await newuser.save()
+
+        // Respond to User
         if (newuser) {
             const tokenObject = utils.issueJWT(user);
             res.status(200).json({
@@ -57,6 +60,7 @@ exports.postAddUser = async (req, res, next) => {
         }
     } catch (err) {
         console.log(err);
+        // sends response for 500 error
         res.status(500).json({
             message: "Sorry, we couldn't complete your request. Please try again in a moment."
         })
@@ -66,7 +70,9 @@ exports.postAddUser = async (req, res, next) => {
 //  User login to the application
 exports.postUserLogin = async (req, res, next) => {
     try {
+        // Get fields from client
         const { username, password } = req.body;
+        // Check fi username exist
         const user = await User.findOne({
             username: username
         })
@@ -75,8 +81,10 @@ exports.postUserLogin = async (req, res, next) => {
                 message: 'You entered the wrong credentials!'
             })
         }
+        // Check if password matches
         const isValid = utils.validPassword(password, user.hash, user.salt)
         if (isValid) {
+            // return user object for authentication
             const tokenObject = utils.issueJWT(user);
             res.status(200).json({
                 success: true,
@@ -85,12 +93,14 @@ exports.postUserLogin = async (req, res, next) => {
                 expiresIn: tokenObject.expires
             })
         } else {
+            // Return error for wrong credentials
             return res.status(401).json({
                 message: "You entered the wrong credentials!"
             });
         }
     } catch (err) {
         console.log(err);
+        // sends response for 500 error
         res.status(500).json({
             message: "Sorry, we couldn't complete your request. Please try again in a moment."
         })
